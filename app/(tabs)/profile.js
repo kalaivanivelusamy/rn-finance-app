b0a6b8fffdc04fc610f9877397d800a379ca0cfa
KayVee <kalai.v.vani@gmail.com>
2025-12-08 23:08:21 +0530

    added constants

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NetflixIcon } from '@/constants/icons'
 
 export default function profile() {
   return (
     <View>
       <Text>profile</Text>
+      {/* <NetflixIcon  width={50} height={50} color="black"/> */}
     </View>
   )
 }
---
+++ constants/icons.js
@@ -0,0 +1,10 @@
+import DollarIcon from "@/assets/icons/dollar-sign.svg";
+import NetflixIcon from "@/assets/icons/netflix.svg";
+import AmazonIcon from "@/assets/icons/amazon-outlined.svg";
+
+
+export {
+    DollarIcon,
+    NetflixIcon,
+    AmazonIcon
+};
